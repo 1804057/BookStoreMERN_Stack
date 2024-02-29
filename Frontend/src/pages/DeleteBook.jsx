67473d12dd0fd688c79312ef175f5bd8a21b3910
Zerin Shaima Meem <64685823+1804057@users.noreply.github.com>
2024-02-29 14:45:52 +0600

    Update DeleteBook.jsx

--- Frontend/src/pages/DeleteBook.jsx
+++ Frontend/src/pages/DeleteBook.jsx
@@ -15,7 +15,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`book-store-mern-stack-backend-three.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Deleted successfully", { variant: "success" });
