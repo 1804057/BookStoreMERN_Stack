bd3bca6587b3ec5d9712f0cb580e2377be1ad75e
1804057 <u1804057@student.cuet.ac.bd>
2024-02-29 14:34:10 +0600

    vercel.js added

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,14 @@ const app=express();
 
 //Middleware for parsing request body
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 app.get('/',(request, response)=>{
     console.log(request);
     return response.status(234).send('Welcome to bookstore');
