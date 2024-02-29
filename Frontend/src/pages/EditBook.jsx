2271409226612a1af758aaad2ac8930077d2583b
Zerin Shaima Meem <64685823+1804057@users.noreply.github.com>
2024-02-29 16:01:33 +0600

    Update EditBook.jsx

--- Frontend/src/pages/EditBook.jsx
+++ Frontend/src/pages/EditBook.jsx
@@ -18,7 +18,7 @@ const EditBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`book-store-mern-stack-backend-dun.vercel.app/books/${id}`)
+      .get(`https://book-store-mern-stack-backend-dun.vercel.app/books/${id}`)
       .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear);
